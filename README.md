f48975a11c1348f5f82c42c4face52b34f159d7e
francho <lab@francho.org>
2013-12-11 12:04:58 +0100

    Initial import

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-generator-javascript-kata
-=========================
+# generator-javascript-kata [![Build Status](https://secure.travis-ci.org/francho/generator-javascript-kata.png?branch=master)](https://travis-ci.org/francho/generator-javascript-kata)
 
-Yeoman generator for javascript katas
+A [Yeoman](http://yeoman.io) generator for javascript katas.
+
+## Usage
+
+Install `generator-javascript-kata`:
+```
+npm install -g generator-javascript-kata
+```
+
+Make a new directory, and `cd` into it:
+```
+mkdir my-new-project && cd $_
+```
+
+Run `yo angular`
+```
+yo javascript-kata 
+
+Run `karma start` for start tests runner
+
+You must put your code into ./app/src folder and your tests under ./test/spec folder
+
+## Libraries included by default
+
+* [chaijs](http://chaijs.com/)
+* [underscore](http://underscorejs.org/)
+
+### Getting To Know Yeoman
+
+Yeoman has a heart of gold. He's a person with feelings and opinions, but he's very easy to work with. If you think he's too opinionated, he can be easily convinced.
+
+If you'd like to get to know Yeoman better and meet some of his friends, [Grunt](http://gruntjs.com) and [Bower](http://bower.io), check out the complete [Getting Started Guide](https://github.com/yeoman/yeoman/wiki/Getting-Started).
+
---
+++ app/index.js
@@ -0,0 +1,52 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var JavascriptKataGenerator = module.exports = function JavascriptKataGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(JavascriptKataGenerator, yeoman.generators.Base);
+
+JavascriptKataGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // have Yeoman greet the user.
+  console.log(this.yeoman);
+
+  var prompts = [{
+    type: 'confirm',
+    name: 'someOption',
+    message: 'Would you like to enable this option?',
+    default: true
+  }];
+
+  this.prompt(prompts, function (props) {
+    this.someOption = props.someOption;
+
+    cb();
+  }.bind(this));
+};
+
+JavascriptKataGenerator.prototype.app = function app() {
+  this.mkdir('app');
+  this.mkdir('app/src');
+  this.mkdir('test/spec');
+
+  this.copy('_package.json', 'package.json');
+  this.copy('_bower.json', 'bower.json');
+  this.copy('_karma.conf.js', 'karma.conf.js');
+};
+
+JavascriptKataGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+};
